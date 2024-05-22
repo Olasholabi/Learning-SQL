5fff9f311e5790cb54951c46fbce5f88f7b0597c
Gafar Olamiku Sholabi <olasholabi@gmail.com>
2024-05-22 20:25:39 +0100

    Add files via upload

---
+++ CTEs(Common Table Expressions).sql
@@ -0,0 +1,45 @@
+-- CTEs (Common Table Expressions) --
+
+WITH CTE_Example AS
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example
+;
+
+WITH CTE_Example(Gender, AVG_Salary, MAX_Salary, MIN_Salary, Count_Salary)  AS
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT *
+FROM CTE_Example
+;
+
+-- Using multiple CTEs in a block of SQL Code --
+
+WITH CTE_Example AS
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+),
+CTE_Example2 AS
+(
+SELECT employee_id, salary
+FROM employee_salary
+WHERE salary > 50000
+)
+SELECT *
+FROM CTE_Example EX1
+JOIN CTE_Example2 EX2
+	ON EX1.employee_id = EX2.employee_id
+;
